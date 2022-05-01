f66c2183d4cc67f743ea7922f31e2b04bf02cf15
Arnav-Barman <arnavbarman1@gmail.com>
2022-05-01 21:25:26 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,12 +22,12 @@ def english_to_french(english_text):
     translation = language_translator.translate(
     text=english_text,
     model_id='en-fr').get_result()
-    french_text = json.dumps(translation, indent=2, ensure_ascii=False)
+    french_text = translation['translations'][0]['translation']
     return french_text
 
 def french_to_english(french_text):
     translation = language_translator.translate(
     text=french_text,
     model_id='fr-en').get_result()
-    english_text = json.dumps(translation, indent=2, ensure_ascii=False)
+    english_text = translation['translations'][0]['translation']
     return english_text
